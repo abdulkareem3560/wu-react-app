ff23df1d0d54e00d1dabb5fba51a72e5551ef6e1
abdulkareem3560 <abdulkareem3560@gmail.com>
2025-06-27 17:04:11 +0530

    removed nodePolyfills

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills({
-      protocolImports: true, // optional, for node: protocol support
-    }),
   ],
   server: {
     proxy: {
